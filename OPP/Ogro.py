2e5aaf029327aa9d24774da92ebac5c99d1470bc
martinez-cortes-macario <m23796376@gmail.com>
2026-03-02 15:47:50 -0800

    Actualizaion de ogro, zombie, main

--- OPP/Ogro.py
+++ OPP/Ogro.py
@@ -1,4 +1,5 @@
 from Enemigo import *
+import random 
 
 class Ogro(Enemigo):
     def __init__(self, puntos_energia=28, ataque=3):
@@ -6,3 +7,10 @@ class Ogro(Enemigo):
 
     def habla(self):
         print("Ogro aplastar todo!!!")
+
+    def ataque_especial(self):
+        print("Ogro taque especial!!")
+        funcion_ataque_especial = random.random()<0.50
+        if funcion_ataque_especial:
+            self.puntos_energia += 4
+            print("Ogro enojado y incremento su ataque por 4!")
--- OPP/Zombie.py
+++ OPP/Zombie.py
@@ -1,5 +1,5 @@
 from Enemigo import *
-
+import random
 class Zombie(Enemigo):
     def __init__(self, puntos_energia=10, ataque=1):
         super().__init__(Tipos_Enemigos='Zombie', puntos_energia=puntos_energia, ataque=ataque)
@@ -9,3 +9,10 @@ class Zombie(Enemigo):
 
     def propagar_enfermedad(self):
         print("El Zombie esta tratando de propagar la enfermedad!!")
+
+    def ataque_especial(self):
+        print("Zombie taque especial!!")
+        funcion_ataque_especial = random.random()<0.50
+        if funcion_ataque_especial:
+            self.puntos_energia += 2
+            print("Zombie ha regenerado su energia con 2HP!")
